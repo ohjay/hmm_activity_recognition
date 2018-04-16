1592c25431f3ab0d760bb492e8b4cfb8f97ae33b
dankmachine <williamjow67@gmail.com>
2018-04-15 18:18:49 -0700

    minor initial setup for hmm part

--- scripts/build_model.py
+++ scripts/build_model.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
+import numpy as np
 from hmmlearn import hmm
 
-# TODO read from config file
-N_COMPONENTS = 3
+# 
+N_COMPONENTS = 4
+
+# Ininitialize initial transition matrix as per the paper
+TRANSMAT_PRIOR = np.array([[1/3, 1/3, 1/3, 0],
+                           [0,   1/3, 1/3, 1/3],
+                           [0,   0,   1/2, 1/2],
+                           [0,   0,   0,   1]])
 
 # estimate model parameters from observed features
 # potential: http://larsmans.github.io/seqlearn/
-# TODO get features first
-model = hmm.GaussianHMM(n_components=N_COMPONENTS, covariance_type='full')
+model = hmm._BaseHMM(n_components=N_COMPONENTS,
+                     transmat_prior=TRANSMAT_PRIOR,
+                     verbose=True)
