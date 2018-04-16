c4ee8d8082bf726da3101d5dde5aa6783f4917d7
dankmachine <williamjow67@gmail.com>
2018-04-15 23:24:51 -0700

    preliminary HMM builder

--- scripts/build_model.py
+++ scripts/build_model.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import os
 import numpy as np
 from hmmlearn import hmm
-import extract_features
+from sklearn.externals import joblib
+from .extract_features import load_features
 
 # Initialize initial transition matrix as per the paper
 TRANSMAT_PRIOR = np.array([[1/3, 1/3, 1/3, 0],
@@ -12,28 +14,63 @@ TRANSMAT_PRIOR = np.array([[1/3, 1/3, 1/3, 0],
 
 # estimate model parameters from observed features
 # potential: http://larsmans.github.io/seqlearn/
-def learn_params(activity_h5, n_components, transmat_prior=TRANSMAT_PRIOR):
-    """Return an HMM model with learned parameters
-       (transition and emission probabilities) 
+def learn_params(activity_h5, model_file, n_components,
+                 transmat_prior=TRANSMAT_PRIOR):
+    """ Save an HMM model (Gaussian emissions) with learned parameters
+        (transition and emission probabilities) to model_file
 
-       Parameters
-       ----------
-           activity_h4: string
-               filepath for the h5 file containing the feature matrix
-               and sequence length vector associated with the activity
+        Parameters
+        ----------
+            activity_h5: string
+                filepath for the h5 file containing the feature matrix
+                and sequence length vector associated with the activity
 
-           n_components: int
-               the number of states in the model
+            mode_file: string
+                filepath to save file (should have .pkl file extension)
 
-           transmat_prior: array-like, shape (n_components, n_components)
-               prior transition matrix
+            n_components: int
+                number of states in the model
+
+            transmat_prior: array-like, shape (n_components, n_components)
+                prior transition matrix
     """
-    model = hmm._BaseHMM(n_components=n_components,
-                         transmat_prior=transmat_prior,
-                         verbose=True)
-    feature_matrix, seq_lengths = extract_features.load_features(activity_h5)
+    model = hmm.GaussianHMM(n_components=n_components,
+                            transmat_prior=transmat_prior,
+                            init_params = 't', verbose=True)
+    feature_matrix, seq_lengths = load_features(activity_h5)
     model.fit(feature_matrix, seq_lengths)
+    joblib.dump(model, model_file)
     return model
+
+def populate_model_dir(h5_dir, model_dir, n_components,
+                       transmat_prior=TRANSMAT_PRIOR):
+    """ Populate the model directory with trained models corresponding
+        to each h5 file in h5_dir
+
+        Parameters
+        ----------
+            h5_dir: string
+                directory for h5 files
+
+            model_dir: string
+                directory to store models
+
+            n_components: int
+                number of states for each model
+
+            transmat_prior: array-like, shape (n_components, n_components)
+                prior transition matrix for each model
+    """
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    for (dirpath, dirnames, filenames) in os.walk(h5_dir):
+        for filename in filenames:
+            if filename.endswith('.h5'):
+                activity_h5 = os.path.join(h5_dir, filename)
+                activity_pkl = filename[:-3] + '.pkl'
+                model_file = os.path.join(model_dir, activity_pkl)
+                learn_params(activity_h5, model_file, n_components,
+                             transmat_prior)
 
 if __name__ == '__main__':
     learn_params(sys.argv)
