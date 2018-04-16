010002d5ac017e020d3df604368d1f748f3ed429
Owen Jow <owenjow@berkeley.edu>
2018-04-16 01:32:22 -0700

    specify classification in config file

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import yaml
 import argparse
 import scripts.extract_features as ef
 import scripts.build_model as bm
+import scripts.classify_activity as ca
+
+"""
+main.py
+
+To add a new mode, define a function with the signature `<mode_name>(config, options=None)`.
+To run, specify MODE at the top of the config file (along with any mode-specific parameters)
+and execute `main.py` with the config path as an argument.
+
+Mode options:
+- extract_features
+- build_models
+- classify_activity
+"""
 
 def extract_features(config, options=None):
     save_path = options.get('save_path', None)
@@ -22,6 +36,12 @@ def build_models(config, options=None):
     h5_dir = options['h5_dir']
     model_dir = options['model_dir']
     bm.populate_model_dir(h5_dir, model_dir, n_components)
+
+def classify_activity(config, options=None):
+    video_path = options['video_path']
+    model_dir = options['model_dir']
+    activity_probs = ca.get_activity_probs(video_path, model_dir)
+    print(activity_probs)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
